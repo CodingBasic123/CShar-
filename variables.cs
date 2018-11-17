316860e4574d143b146d5e4c68575b6663535930
CodingBasic123 <40132394+CodingBasic123@users.noreply.github.com>
2018-11-17 00:54:57 -0800

    variables.cs
    dedd

--- variables.cs
+++ variables.cs
@@ -1,1 +1,12 @@
+using System;
 
+public class main
+{
+    public static void Main(string[] args)
+    {
+        string jello = "hi";
+        int hello = 10;
+        Console.WriteLine(jello);
+        Console.WriteLine(hello);
+    }
+}
